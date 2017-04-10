86207091d69d24f7692e6ace5025903f84b15f5c
Clint Carr <clint.carr@outlook.com>
2017-04-10 22:02:47 +1000

    Initial Commit

--- QlikSenseRepositoryAPI/Program.cs
+++ QlikSenseRepositoryAPI/Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Text;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 
-namespace QlikSenseRepositoryAPI
+namespace QlikSenseRepository
 {
-    class Program
+    class ConnectQlik
     {
-        static void Main(string[] args)
+        private X509Certificate2 Certificate_ { get; set; }
+
+        public ConnectQlik()
         {
+            // First locate the Qlik Sense client certificate
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly);
+            Certificate_ = store.Certificates.Cast<X509Certificate2>().FirstOrDefault(c => c.FriendlyName == "QlikClient");
+            store.Close();
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
         }
+
+        public string GetRequest(string server, string endpoint)
+        {
+            string url = "https://" + server + ":4242" + endpoint;
+            string Xrfkey = "0123456789abcdef";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + "?Xrfkey=" + Xrfkey);
+            request.ClientCertificates.Add(Certificate_);
+            request.Method = "GET";
+            request.Accept = "application/json";
+            request.Headers.Add("X-Qlik-Xrfkey", Xrfkey);
+            request.Headers.Add("X-Qlik-User", "UserDirectory=INTERNAL; UserID=sa_repository");
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            return stream != null ? new StreamReader(stream).ReadToEnd() : string.Empty;
+        }
+
     }
 }
---
+++ QlikSenseRepositoryAPI/result.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace QlikSenseRepository
+{
+    class Result
+    {
+        private static void Main()
+        {
+            ConnectQlik get = new ConnectQlik();
+            string response = get.GetRequest("qs2.qliklocal.net", "/qrs/about");
+            Console.WriteLine(response);
+            Console.ReadKey(true);
+        }
+    }
+}
